3f955064c2adf55741f34d545bd3c2544682e339
nasaf_khan <nessafhan.kn@gmail.com>
2021-10-12 10:05:38 +0530

    Modified files and added requirements.py file

--- auth/settings.py
+++ auth/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*']
